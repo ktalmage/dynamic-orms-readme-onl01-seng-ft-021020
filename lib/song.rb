81f3c5225e73f00bd967404b43beae6571cc419e
Kyle Talmage <ktalmage85@gmail.com>
2020-03-14 13:39:43 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,12 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
-
+require 'pry'
 class Song
 
 
   def self.table_name
     self.to_s.downcase.pluralize
+    binding.pry
   end
 
   def self.column_names
