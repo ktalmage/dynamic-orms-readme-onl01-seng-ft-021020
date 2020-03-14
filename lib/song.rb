9c2cafd7a008b0d7f757b9c029000684416d4321
Kyle Talmage <ktalmage85@gmail.com>
2020-03-14 13:59:42 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,12 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
-
+require 'pry'
 class Song
 
 
   def self.table_name
     self.to_s.downcase.pluralize
+  
   end
 
   def self.column_names
@@ -19,6 +20,7 @@ class Song
       column_names << row["name"]
     end
     column_names.compact
+    
   end
 
   self.column_names.each do |col_name|
